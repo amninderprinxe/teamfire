231b2926ca9dc659b3693e0c26af464c45805586
amninderprinxe <amninder99155@gmail.com>
2025-06-30 11:57:59 +0530

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/teamfire/',
   plugins: [react()],
+  base: '/teamfire/' // important for GitHub Pages
 })
